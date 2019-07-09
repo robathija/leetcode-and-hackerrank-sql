9ba8e7d95dc6f8079a6eeb4069cc6ad1b8a4cb25
Rohit Bathija <rohit3991@gmail.com>
2019-07-09 15:28:26 +0900

    Solution
    Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.

---
+++ top-competitors.sql
@@ -0,0 +1,12 @@
+SELECT h.hacker_id, h.name 
+FROM Submissions AS s 
+INNER JOIN Hackers AS h 
+ON s.hacker_id = h.hacker_id
+INNER JOIN Challenges AS c 
+ON s.challenge_id = c.challenge_id
+INNER JOIN Difficulty AS d 
+ON c.difficulty_level = d.difficulty_level
+WHERE s.score = d.score 
+GROUP BY h.hacker_id, h.name 
+HAVING COUNT(*)>1 
+ORDER BY COUNT(*) DESC, h.hacker_id;
